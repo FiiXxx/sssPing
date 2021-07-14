7350ee650c745dcde4b2bdf4ba445a07b3cd938f
FiiXxx <84270777+FiiXxx@users.noreply.github.com>
2021-07-14 12:25:33 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,32 +9,66 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setTitle(`**Hyper Music  Commands**`)
+    .setTitle(`**Only Music  Commands**`)
     .setThumbnail("https://media.tenor.com/images/3145445a2c5bde03ac708c2c309fdbca/tenor.gif")
-    .setAuthor("Prefix [ $ ]","")
+    .setAuthor("Prefix [ + ]","")
     .setDescription(` 
 \`Help Commands\`
 
-**Music Command**
+help
+List of all commands
+• ────── emoji_102 ────── •
+☢◢User Commands
++invite - +ping 
++stats - +support
++hosting - +docs
++howtohost - +djs
 
-\` $paly , $stop , $skip , $skipto , $volume \`
+• ────── RAES ────── •
+☢◢Music Commands
++addprevious - +addsimilar 
++autoplay - +join
++jump - +loob 
++loobqueue - +lyrics
++move - +moveme 
++nowplaying - +pause
++play(p) - +playlist
++playsc - +play skip 
++queue - +radio
++search - +shuffle 
++stop - +volume 
+────── Sixo ────── •
++8ball - +delete
++fact - +joke
++kiss - +meme
++rip - +shit
++say - +purge
+• ────── RAES ────── •
++addbotchat - +adddj
++prefix - +removebotchat 
++reset - settings - +setup 
++toggledjoinly - +toggleplaymssnge
 
-\` $queue , $loop , $pause , $nowplaying \`
+• ────── emoji_97 ────── •
+☢◢Moderator Commands
++avatar - +botinfo - +help
++invite - +ping - +reactions
++serverinfo - +stats - +userinfo 
++uptime - +ban - +kick 
++embed - +slawmode
++unmute - +mute
++unwarn - +warn - +warnings 
 
-\` $shuffle , $uptime , $search , $remove \`
+• ────── FILTER────── •
+☢◢Owner Commands
++bassboost - +cleareq 
++clearfilter - +equalizer 
 
-\` $clip , $clips , $lyrics , $move , $playlist \`
-
-**Info Command**
-
-\` $bot , $support , $invite , $help , $ping \`
 
 
 
      
-**[ invite ](https://discord.com/api/oauth2/authorize?client_id=780578520870158337&permissions=8&scope=bot)** , **[ support ](https://discord.gg/3wykSpqjZq)**      
-
-**[ vote ](https://top.gg/bot/780578520870158337)**                                                                
+**[ invite ](https://discord.com/api/oauth2/authorize?client_id=864798677783281724&permissions=8&scope=bot)** , **[ support ](https://discord.gg/8b6pF9CV)**                                                                      
  `)
     
     helpEmbed.setTimestamp();
